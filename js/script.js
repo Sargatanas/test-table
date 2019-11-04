7f7dd10f867bcf14d53d1307bec384820b23339a
Silvamorta <sargatanas94@mail.ru>
2019-11-04 18:00:35 +0500

    Добавлено: пролистывание строк без пагинации (кнопки)

--- js/script.js
+++ js/script.js
@@ -2,21 +2,19 @@
     // Начальные данные
     let config = {
         array: JSON.parse(localStorage.getItem('data')),
-        page: 1,
         width: Number(document.getElementById('h-size').value),
         height: Number(document.getElementById('v-size').value),
-        getShift: 0
+        shiftX: 0,
+        shiftY: 0
     };
 
     // Первичный вывод таблицы
-    createPages(config);
     constructTable(config);
 
     // Обновление таблицы
     document.getElementById('v-size').addEventListener('change', function () {
         config.height = Number(document.getElementById('v-size').value);
         config.page = 1;
-        createPages(config);
         constructTable(config);
     });
 
@@ -27,45 +25,20 @@
     });
 })();
 
-
-// Вывод пагинации
-function createPages(config) {
-    let countPage = Math.ceil(config.array.countStr / config.height);
-
-    let pages = document.getElementById('pages');
-    pages.innerHTML = '';
-
-    for (let i = 1; i <= countPage; i++) {
-        let optionPage = document.createElement('option');
-        optionPage.value = i;
-        if (i === 1) {
-            optionPage.setAttribute('selected','true');
-        }
-        optionPage.innerText = i.toString();
-
-        pages.appendChild(optionPage);
-    }
-
-    pages.addEventListener('change', function () {
-        config.page = Number(pages.value);
-        constructTable(config);
-    });
-}
-
 // Отрисовка таблицы
 function constructTable(config) {
     let table = document.getElementById('table');
     table.innerText = '';
 
     // Расчет границ по высоте
-    let firstStringNum = config.height * (config.page - 1);
-    let lastStringNum = config.height * config.page;
+    let firstStringNum = config.shiftY;
+    let lastStringNum = firstStringNum + config.height - 1;
     lastStringNum = Math.min(lastStringNum, config.array.countStr);
     config['strStart'] = firstStringNum;
     config['strEnd'] = lastStringNum;
 
     // расчет границ по ширине
-    let firstColumnNum = config.getShift + 1;
+    let firstColumnNum = config.shiftX + 1;
     let lastColumnNum = firstColumnNum + config.width - 1;
     lastColumnNum = Math.min(lastColumnNum, config.array.countCol);
     config['colStart'] = firstColumnNum;
@@ -73,10 +46,13 @@ function constructTable(config) {
 
     table.appendChild(constructTableHeader(config));
 
+    table.appendChild(setButtonTop(config));
+
     constructTableBody(config).forEach(function(tableRow) {
         table.appendChild(tableRow);
     });
 
+    table.appendChild(setButtonBottom(config));
 }
 
 // Отрисовка шапки таблицы
@@ -93,8 +69,8 @@ function constructTableHeader(config) {
 
     row.appendChild(cell);
 
-    // Добавление кнопки прокрутки столбцов влево
-    row.appendChild(setButtonLeft(config));
+    cell = document.createElement('th');
+    row.appendChild(cell);
 
     for (let i = config.colStart; i <= config.colEnd; i++) {
         cell = document.createElement('th');
@@ -105,15 +81,15 @@ function constructTableHeader(config) {
         row.appendChild(cell);
     }
 
-    // Добавление кнопки прокрутки столбцов вперед
-    row.appendChild(setButtonRight(config));
+    cell = document.createElement('th');
+    row.appendChild(cell);
 
     return row;
 }
 
 // Добавление кнопки прокрутки влево
 function setButtonLeft(config) {
-    let cell = document.createElement('th');
+    let cell = document.createElement('td');
     cell.rowSpan = config.height + 1;
     cell.classList.add('table-main-content__cell');
     cell.classList.add('table-main-content__cell_button');
@@ -123,8 +99,8 @@ function setButtonLeft(config) {
     cellButton.classList.add('table-main-content__button_left');
 
     cellButton.addEventListener('click', function () {
-        if ( config.getShift !== Math.max(0, config.getShift - 1)) {
-            config.getShift--;
+        if ( config.shiftX !== Math.max(0, config.shiftX - 1)) {
+            config.shiftX--;
             constructTable(config);
         }
     });
@@ -135,7 +111,7 @@ function setButtonLeft(config) {
 
 // Добавление кнопки прокрутки вправо
 function setButtonRight(config) {
-    let cell = document.createElement('th');
+    let cell = document.createElement('td');
     cell.rowSpan = config.height + 1;
     cell.classList.add('table-main-content__cell');
     cell.classList.add('table-main-content__cell_button');
@@ -145,14 +121,78 @@ function setButtonRight(config) {
     cellButton.classList.add('table-main-content__button_right');
 
     cellButton.addEventListener('click', function () {
-        if ( config.getShift !== Math.min(config.getShift + 1, config.array.countCol - config.width)) {
-            config.getShift++;
+        if ( config.shiftX !== Math.min(config.shiftX + 1, config.array.countCol - config.width)) {
+            config.shiftX++;
             constructTable(config);
         }
     });
     cell.appendChild(cellButton);
 
     return cell;
+}
+
+// Добавление кнопки прокрутки вверх
+function setButtonTop(config) {
+    let row = document.createElement('tr');
+
+    let cell = document.createElement('td');
+    row.appendChild(cell);
+
+    row.appendChild(setButtonLeft(config));
+
+    cell = document.createElement('td');
+    cell.colSpan = config.width + 3;
+    cell.classList.add('table-main-content__cell');
+    cell.classList.add('table-main-content__cell_button');
+
+    let cellButton  = document.createElement('button');
+    cellButton.classList.add('table-main-content__button');
+    cellButton.classList.add('table-main-content__button_top');
+
+    cellButton.addEventListener('click', function () {
+        if ( config.shiftY !== Math.max(0, config.shiftY - 1)) {
+            config.shiftY--;
+            constructTable(config);
+        }
+    });
+    cell.appendChild(cellButton);
+
+    row.appendChild(cell);
+
+    row.appendChild(setButtonRight(config));
+
+    return row;
+}
+
+// Добавление кнопки прокрутки вниз
+function setButtonBottom(config) {
+    let row = document.createElement('tr');
+
+    let cell = document.createElement('td');
+    row.appendChild(cell);
+
+    cell = document.createElement('td');
+    cell.colSpan = config.width + 3;
+    cell.classList.add('table-main-content__cell');
+    cell.classList.add('table-main-content__cell_button');
+
+    let cellButton  = document.createElement('button');
+    cellButton.classList.add('table-main-content__button');
+    cellButton.classList.add('table-main-content__button_bottom');
+
+    cellButton.addEventListener('click', function () {
+        if ( config.shiftY !== Math.min(config.shiftY + 1, config.array.countStr - config.height)) {
+            config.shiftY++;
+            constructTable(config);
+        }
+    });
+    cell.appendChild(cellButton);
+    row.appendChild(cell);
+
+    cell = document.createElement('td');
+    row.appendChild(cell);
+
+    return row;
 }
 
 // Отрисовка контента таблицы
@@ -177,6 +217,7 @@ function constructTableBody(config) {
 
             row.appendChild(cell);
         }
+
         body.push(row);
     }
 
