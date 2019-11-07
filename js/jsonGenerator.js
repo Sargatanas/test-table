84911d761109ad20ae02c20bdacad3cf40b24c76
Silvamorta <sargatanas94@mail.ru>
2019-11-07 19:38:24 +0500

    Добавлено: прелоадер

--- js/jsonGenerator.js
+++ js/jsonGenerator.js
@@ -1,35 +1,8 @@
 (function jsonGenerator () {
-    let str = 1000;
-    let col = 20;
+    let request = new XMLHttpRequest();
+    request.open('POST', './js/php/createJson.php', false);
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    request.send();
 
-    let names = ['Иван', 'Федор', 'Михаил', 'Алексей', 'Дмитрий', 'Пётр', 'Александр'];
-    let surnames = ['Бабочкин', 'Синичкин', 'Кесьянов', 'Иванов', 'Васиьев', 'Теркин', 'Работяга'];
-    let patronymics = ['Иванович', 'Федорович', 'Алексеевич', 'Иванович', 'Васиьевич', 'Петрович', 'Александрович'];
-
-    let table = {
-        countStr: str,
-        countCol: col,
-        data: []
-    };
-
-    for (let i = 0; i < str; i++) {
-        let name = surnames[randomIndex(0, surnames.length)];
-        name += ' ' + names[randomIndex(0, names.length)];
-        name += ' ' + patronymics[randomIndex(0, patronymics.length)];
-
-        table.data[i] = {
-            id: i,
-            name: name,
-            facts: {}
-        };
-        for (let j = 1; j <= col; j++) {
-            table.data[i].facts['Fact_' + j] = 'Значение ' + (i + 1) + '.' + j;
-        }
-    }
-
-    localStorage.setItem('data', JSON.stringify(table));
+    console.log(request.response);
 })();
-
-function randomIndex(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-}
---
+++ js/php/createJson.php
@@ -0,0 +1,88 @@
+<?php
+set_time_limit(0);
+
+$url = $_SERVER['DOCUMENT_ROOT'] . '/resources/json/data.json';
+$file = fopen($url, 'a');
+
+$str = 10000;
+$col = 1000;
+
+$names = ['Иван', 'Федор', 'Михаил', 'Алексей', 'Дмитрий', 'Пётр', 'Александр'];
+$surnames = ['Бабочкин', 'Синичкин', 'Кесьянов', 'Иванов', 'Васиьев', 'Теркин', 'Работяга'];
+$patronymics = ['Иванович', 'Федорович', 'Алексеевич', 'Иванович', 'Васиьевич', 'Петрович', 'Александрович'];
+
+$string = '{' .
+    '"countStr": ' . $str . ',' .
+    '"countCol": '. $col . ',' .
+    '"data": [';
+fwrite($file, $string);
+
+$temp_str = -1;
+$temp_col = -1;
+
+for ($i = 0; $i < $str - 1; $i++) {
+    $temp_col = -1;
+    $name = $surnames[rand(0, count($surnames) - 1)];
+    $name .= ' ' . $names[rand(0, count($names) - 1)];
+    $name .= ' ' . $patronymics[rand(0, count($patronymics) - 1)];
+
+    $string = '{' .
+        '"id": ' . $i . ',' .
+        '"name": "'. $name . '",' .
+        '"facts": {';
+    fwrite($file, $string);
+
+    for ($j = 1; $j <= $col - 1; $j++) {
+        $key = 'Fact_' . $j;
+        $string = '"' . $key . '": "Значение ' . ($i + 1) . '.' . $j . '",';
+        fwrite($file, $string);
+        $temp_col++;
+    }
+    $key = 'Fact_' . $j;
+    $string = '"' . $key . '": "Значение ' . ($i + 1) . '.' . $j . '"';
+    fwrite($file, $string);
+    $temp_col++;
+
+    $string = '}},';
+    fwrite($file, $string);
+
+    $temp_str++;
+}
+
+
+$temp_col = -1;
+$name = $surnames[rand(0, count($surnames))];
+$name .= ' ' . $names[rand(0, count($names))];
+$name .= ' ' . $patronymics[rand(0, count($patronymics))];
+
+$string = '{' .
+    '"id": ' . $i . ',' .
+    '"name": "'. $name . '",' .
+    '"facts": {';
+fwrite($file, $string);
+
+for ($j = 1; $j <= $col - 1; $j++) {
+    $key = 'Fact_' . $j;
+    $string = '"' . $key . '": "Значение ' . ($i + 1) . '.' . $j . '",';
+    fwrite($file, $string);
+    $temp_col++;
+}
+$key = 'Fact_' . $j;
+$string = '"' . $key . '": "Значение ' . ($i + 1) . '.' . $j . '"';
+fwrite($file, $string);
+$temp_col++;
+
+$string = '}}';
+fwrite($file, $string);
+
+$temp_str++;
+
+
+
+$string = ']}';
+fwrite($file, $string);
+
+fclose($file);
+
+echo $temp_str;
+echo $temp_col;
